91cd65cd6568000bf4bd61a2c02d2133d0ad0e78
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-12-15 11:46:21 +0530

    fix: shuttle configs

--- shuttle_run.sh
+++ shuttle_run.sh
@@ -1,9 +1,6 @@
-
-
 PORT=3000
 NODE_ENV=development
-npm install 
+npm install typescript -g
+npm install
 NODE_ENV=production
 npm start
-
-
--- shuttle_setup_container.sh
+++ shuttle_setup_container.sh
@@ -5,4 +5,7 @@ bash nodesource_setup.sh
 apt-get update
 echo "installing packages ..."
 apt-get install -y libfontconfig1 ttf-ancient-fonts nodejs
+echo "installing rust ..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 echo "---- shuttle_setup_container.sh done executing ----"
